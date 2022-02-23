7891ee1ac29841a174ee688bc9f65c2159e40d97
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2022-02-23 09:59:56 -0500

    temp save

--- apps/wave/run.sh
+++ apps/wave/run.sh
@@ -1,6 +1,6 @@
 echo "Start: $(date)"
 echo "cwd: $(pwd)"
 
-mpirun -n 16 ~/projects/raccoon/raccoon-opt -i elastodynamic.i
+mpirun -n 4 ~/projects/raccoon/raccoon-opt -i elastodynamic.i
 
 echo "End: $(date)"
