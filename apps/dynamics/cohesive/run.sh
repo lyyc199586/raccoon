ace9fa476403855ede61d073cf558ec96e87dd37
yuanchen95 <liuyangyuanchen@outlook.com>
2022-03-04 17:03:38 -0500

    update

--- apps/dynamics/cohesive/run.sh
+++ apps/dynamics/cohesive/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name Dynamics
+#SBATCH --job-name SWL3D
 #SBATCH -o output.out
 #SBATCH -e error.err
 #SBATCH -N 4
 #SBATCH --ntasks-per-node=128
-#SBATCH -t 12:00:00
+#SBATCH -t 24:00:00
 
 echo "Start: $(date)"
 echo "cwd: $(pwd)"
