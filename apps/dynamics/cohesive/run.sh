17aa527441f1c0c6b95572165a560221379d33df
yuanchen95 <liuyangyuanchen@outlook.com>
2022-03-04 14:18:27 -0500

    update run.sh

--- apps/dynamics/cohesive/run.sh
+++ apps/dynamics/cohesive/run.sh
@@ -2,14 +2,13 @@
 #SBATCH --job-name Dynamics
 #SBATCH -o output.out
 #SBATCH -e error.err
-#SBATCH -N 2 
+#SBATCH -N 4
 #SBATCH --ntasks-per-node=128
-#SBATCH -t 04:00:00
+#SBATCH -t 12:00:00
 
 echo "Start: $(date)"
 echo "cwd: $(pwd)"
 
-source $HOME/.moose_profile
 mpirun -n $SLURM_NPROCS ~/projects/raccoon/raccoon-opt -i elasticity.i
 
 echo "End: $(date)"
