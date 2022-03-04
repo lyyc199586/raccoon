230c909445fd3f43bdcee40b8472aadbf1730087
yuanchen95 <liuyangyuanchen@outlook.com>
2022-03-04 14:18:27 -0500

    update run.sh

--- apps/dynamics/cohesive/run.sh
+++ apps/dynamics/cohesive/run.sh
@@ -3,6 +3,7 @@
 #SBATCH -o output.out
 #SBATCH -e error.err
 <<<<<<< HEAD
+<<<<<<< HEAD
 #SBATCH -N 4
 #SBATCH --ntasks-per-node=128
 #SBATCH -t 12:00:00
@@ -11,14 +12,22 @@
 #SBATCH --ntasks-per-node=128
 #SBATCH -t 04:00:00
 >>>>>>> first run on bridges2
+=======
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=128
+#SBATCH -t 12:00:00
+>>>>>>> update run.sh
 
 echo "Start: $(date)"
 echo "cwd: $(pwd)"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 source $HOME/.moose_profile
 >>>>>>> first run on bridges2
+=======
+>>>>>>> update run.sh
 mpirun -n $SLURM_NPROCS ~/projects/raccoon/raccoon-opt -i elasticity.i
 
 echo "End: $(date)"
