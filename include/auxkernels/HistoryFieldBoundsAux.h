069d9878218506cc28ccd72447eb8a820fc65ed5
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2022-03-18 20:10:50 -0400

    add history field files

---
+++ include/auxkernels/HistoryFieldBoundsAux.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "MooseTypes.h"
+#include "BoundsAuxBase.h"
+
+/**
+ *
+ */
+class HistoryFieldBoundsAux : public BoundsAuxBase
+{
+public:
+  static InputParameters validParams();
+
+  HistoryFieldBoundsAux(const InputParameters & parameters);
+
+protected:
+  virtual Real getBound() override;
+
+  /// The value of the fixed bound for the variable
+  Real _fixed_bound_value;
+
+  /// The threshold for conditional bound for the variable
+  Real _threshold_ratio;
+
+  /// The search radius for the maximum history field value
+  Real _search_radius;
+
+  /// Name of history field variable
+  NonlinearVariableName _hist_var_name;
+
+  /// history field variable
+  MooseVariable & _hist_var;
+
+  /// The Mesh
+  // const MooseMesh & _mesh;
+
+  bool _first;
+
+  // neighbor nodes map
+  std::map<dof_id_type, std::vector<dof_id_type>> _node_to_near_nodes_map;
+};
