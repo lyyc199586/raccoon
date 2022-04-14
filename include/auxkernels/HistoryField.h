65217b46771dca760f6bf836dd581c2cb49275ea
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2022-04-14 19:45:21 -0400

    update historyfield

--- include/auxkernels/HistoryField.h
+++ include/auxkernels/HistoryField.h
@@ -8,13 +8,12 @@
 class HistoryField : public AuxKernel
 {
 public:
+  static InputParameters validParams();
+
   HistoryField(const InputParameters & parameters);
 
 protected:
   virtual Real computeValue();
 
   const VariableValue & _source_var;
-
-public:
-  static InputParameters validParams();
 };
