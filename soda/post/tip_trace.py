a19b74a227865b00b2f8df3a865a4f96ea07ec58
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2023-06-26 16:36:31 -0400

    update post

--- soda/post/tip_trace.py
+++ soda/post/tip_trace.py
@@ -7,7 +7,10 @@ import numpy as np
 x = pd.read_csv("../gold/exp_tip_x.csv", header=None)
 # r = pd.read_csv("../gold/exp_tip_r.csv", header=None)
 
-df = pd.read_csv("../gold/tip_full_tip_p22.5_gc8.89e-3_ts30_cs330_l0.25_delta0.csv")
+df1 = pd.read_csv("../gold/tip_full_p20_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
+df2 = pd.read_csv("../gold/tip_full_p25_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
+df3 = pd.read_csv("../gold/tip_full_p20_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
+df4 = pd.read_csv("../gold/tip_full_p25_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
 
 
 
@@ -15,9 +18,14 @@ df = pd.read_csv("../gold/tip_full_tip_p22.5_gc8.89e-3_ts30_cs330_l0.25_delta0.c
 fig, ax = plt.subplots(figsize=(3.2, 2.655))
 
 plt.style.use('elsevier.mplstyle')
+plt.tight_layout()
 
-ax.plot(x.iloc[:, 0]+27, x.iloc[:, 1]-26.08, 'x', label='exp')
-ax.plot(df["tip_x"], df["tip_y"], label='sim')
+colors = ['#c1272d', '#0000a7', '#eecc16', '#008176', "#b3b3b3"]
+ax.plot(x.iloc[:, 0]+27, x.iloc[:, 1]-26.08, 'x', label='Experiment', color='grey')
+ax.plot(df1["tip_x"], df1["tip_y"], label='$G_c=9$ N/m, $p=20$ MPa', color=colors[0])
+ax.plot(df2["tip_x"], df2["tip_y"], label='$G_c=9$ N/m, $p=25$ MPa', color=colors[1])
+ax.plot(df3["tip_x"], df3["tip_y"], label='$G_c=10$ N/m, $p=20$ MPa', color=colors[2])
+ax.plot(df4["tip_x"], df4["tip_y"], label='$G_c=10$ N/m, $p=25$ MPa', color=colors[3])
 
 ax.set_xlabel('$x$-axis')
 ax.set_ylabel('$y$-axis')
--- soda/post/tip_vel.py
+++ soda/post/tip_vel.py
@@ -2,12 +2,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.ndimage import uniform_filter1d
 
-def smooth(data, window_size):
-    return np.convolve(data, np.ones(window_size)/window_size, mode='same')
+def plot_df(df, window_size, cR, color, label=''):
+    t = df["time"]*1e6 # to us
+    v = df["tip_velocity"]
+    y = df["tip_y"]
+    t0 = t.loc[df[v > 0].index[0]] # crack initiation time
+    t1 = t.loc[df[y > 0.25].index[0]] # branch start time
+    t2 = t.loc[df[y > 0.5].index[0]] # branch end time
+    ax.plot(t - t0, uniform_filter1d(v, w)/cR, color=color, label=label)
+    ax.axvspan(t1 - t0, t2 - t0, facecolor=color, alpha=0.1)
+
 
 fig, ax = plt.subplots(figsize=(6.4, 2.655))
 plt.style.use('elsevier.mplstyle')
+# plt.tight_layout()
 
 # experiement data
 s1 = pd.read_csv("../gold/exp_s1.csv", header=None)
@@ -21,12 +31,27 @@ y2 = s2.iloc[:, 1]
 
 y_max = np.maximum(y1, y2)/3200
 y_min = np.minimum(y1, y2)/3200
-ax.fill_between(x1, y_min, y_max, facecolor='grey', alpha=0.8, label="exp")
+ax.fill_between(x1, y_min, y_max, facecolor='grey', alpha=0.6, label="Experiment")
 
 # simulation
-df = pd.read_csv("../gold/tip_full_tip_p22.5_gc8.89e-3_ts30_cs330_l0.25_delta0.csv")
-smooth_y = smooth(df["tip_velocity"], 10)
-ax.plot(df["time"]*1e6 - 15, smooth_y/3.2e6, label="sim")
+df1 = pd.read_csv("../gold/tip_full_p20_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
+df2 = pd.read_csv("../gold/tip_full_p25_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
+df3 = pd.read_csv("../gold/tip_full_p20_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
+df4 = pd.read_csv("../gold/tip_full_p25_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
+
+w = 10
+cR = 3.2e6
+# ax.plot(df1["time"]*1e6 - 15.45, uniform_filter1d(df1["tip_velocity"], w)/3.2e6, label='$G_c=9$ N/m, $p=20$ MPa')
+# ax.plot(df2["time"]*1e6 - 14, uniform_filter1d(df2["tip_velocity"], w)/3.2e6, label='$G_c=9$ N/m, $p=25$ MPa')
+# ax.plot(df3["time"]*1e6 - 15.7, uniform_filter1d(df3["tip_velocity"], w)/3.2e6, label='$G_c=10$ N/m, $p=20$ MPa')
+# ax.plot(df4["time"]*1e6 - 14.15, uniform_filter1d(df4["tip_velocity"], w)/3.2e6, label='$G_c=10$ N/m, $p=25$ MPa')
+# colors = ax._get_lines.color_cycle
+colors = ['#c1272d', '#0000a7', '#eecc16', '#008176', "#b3b3b3"]
+dfs = [df1, df2, df3, df4]
+plot_df(dfs[0], w, cR, colors[0], label="$G_c=9$ N/m, $p=20$ MPa")
+plot_df(dfs[1], w, cR, colors[1], label="$G_c=9$ N/m, $p=25$ MPa")
+plot_df(dfs[2], w, cR, colors[2], label="$G_c=10$ N/m, $p=20$ MPa")
+plot_df(dfs[3], w, cR, colors[3], label="$G_c=10$ N/m, $p=25$ MPa")
 
 ax.set_xlim([0, 50])
 ax.set_ylim([0, 0.8])
@@ -35,5 +60,5 @@ ax.set_ylabel("Normalized crack velocity $v/c_R$")
 ax.legend()
 # %%
 
-fig.savefig('tip_vel.png')
+fig.savefig('tip_vel_w10.png')
 # %%
