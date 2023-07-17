2dd399518caa2d4357bfdbefe7772236c4f2043b
lyyc199586 <liuyangyuanchen@outlook.com>
2023-07-17 09:09:46 -0500

    add results

--- soda/post/tip_trace.py
+++ soda/post/tip_trace.py
@@ -7,10 +7,10 @@ import numpy as np
 x = pd.read_csv("../gold/exp_tip_x.csv", header=None)
 # r = pd.read_csv("../gold/exp_tip_r.csv", header=None)
 
-df1 = pd.read_csv("../gold/tip_full_p20_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
-df2 = pd.read_csv("../gold/tip_full_p25_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
-df3 = pd.read_csv("../gold/tip_full_p20_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
-df4 = pd.read_csv("../gold/tip_full_p25_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
+df1 = pd.read_csv("../gold/tip_half_tan_p20_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
+# df2 = pd.read_csv("../gold/tip_full_p25_gc9e-3_ts30_cs330_l0.25_delta-0.625.csv")
+# df3 = pd.read_csv("../gold/tip_full_p20_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
+# df4 = pd.read_csv("../gold/tip_full_p25_gc10e-3_ts30_cs330_l0.25_delta-0.55.csv")
 
 
 
@@ -23,9 +23,9 @@ plt.tight_layout()
 colors = ['#c1272d', '#0000a7', '#eecc16', '#008176', "#b3b3b3"]
 ax.plot(x.iloc[:, 0]+27, x.iloc[:, 1]-26.08, 'x', label='Experiment', color='grey')
 ax.plot(df1["tip_x"], df1["tip_y"], label='$G_c=9$ N/m, $p=20$ MPa', color=colors[0])
-ax.plot(df2["tip_x"], df2["tip_y"], label='$G_c=9$ N/m, $p=25$ MPa', color=colors[1])
-ax.plot(df3["tip_x"], df3["tip_y"], label='$G_c=10$ N/m, $p=20$ MPa', color=colors[2])
-ax.plot(df4["tip_x"], df4["tip_y"], label='$G_c=10$ N/m, $p=25$ MPa', color=colors[3])
+# ax.plot(df2["tip_x"], df2["tip_y"], label='$G_c=9$ N/m, $p=25$ MPa', color=colors[1])
+# ax.plot(df3["tip_x"], df3["tip_y"], label='$G_c=10$ N/m, $p=20$ MPa', color=colors[2])
+# ax.plot(df4["tip_x"], df4["tip_y"], label='$G_c=10$ N/m, $p=25$ MPa', color=colors[3])
 
 ax.set_xlabel('$x$-axis')
 ax.set_ylabel('$y$-axis')
