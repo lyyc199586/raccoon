dd351816787695a3880e95cca34cf49112dd0a0b
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2023-07-17 10:56:33 -0400

    add surf results

--- surf/run_all.sh
+++ surf/run_all.sh
@@ -1,4 +1,4 @@
-for gc in 8e-3 8.5e-3 9e-3 9.5e-3 10e-3; do
+for gc in 12e-3 15e-3; do
   echo Gc=${gc}
-  mpirun -n 3 ~/projects/raccoon/raccoon-opt -i elasticity.i Gc=${gc} > log_gc${gc}.txt 2>&1 &
+  mpirun -n 6 ~/projects/raccoon/raccoon-opt -i elasticity.i Gc=${gc} > log_gc${gc}.txt 2>&1 &
 done
