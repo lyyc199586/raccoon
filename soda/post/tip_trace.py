5e2527e6eaeead1701fa88b8dab2262eba8bbcfc
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2023-07-17 15:23:06 -0400

    update post files

--- soda/post/tip_trace.py
+++ soda/post/tip_trace.py
@@ -21,16 +21,17 @@ plt.style.use('elsevier.mplstyle')
 plt.tight_layout()
 
 colors = ['#c1272d', '#0000a7', '#eecc16', '#008176', "#b3b3b3"]
-ax.plot(x.iloc[:, 0]+27, x.iloc[:, 1]-26.08, 'x', label='Experiment', color='grey')
-ax.plot(df1["tip_x"], df1["tip_y"], label='$G_c=9$ N/m, $p=20$ MPa', color=colors[0])
+ax.plot(x.iloc[:, 0]+35, x.iloc[:, 1]-26.08, 'x', label='Experiment', color='grey')
+ax.plot(df1["tip_x"], df1["tip_y"], label='$\mathcal{G}_c=9$ N/m, $p=20$ MPa', color=colors[0])
 # ax.plot(df2["tip_x"], df2["tip_y"], label='$G_c=9$ N/m, $p=25$ MPa', color=colors[1])
 # ax.plot(df3["tip_x"], df3["tip_y"], label='$G_c=10$ N/m, $p=20$ MPa', color=colors[2])
 # ax.plot(df4["tip_x"], df4["tip_y"], label='$G_c=10$ N/m, $p=25$ MPa', color=colors[3])
 
 ax.set_xlabel('$x$-axis')
 ax.set_ylabel('$y$-axis')
-ax.set_xlim([25, 86])
+ax.set_xlim([25, 100])
 ax.set_ylim([-25, 25])
+ax.set_aspect('equal')
 ax.legend()
 # %%
 
