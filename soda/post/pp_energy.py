0216a988d8c3d752a94866b2b8c99d55d662e866
Yangyuanchen Liu <liuyangyuanchen@outlook.com>
2023-08-07 13:46:38 -0400

    post update

--- soda/post/pp_energy.py
+++ soda/post/pp_energy.py
@@ -12,18 +12,25 @@ pp = pd.read_csv("../gold/pp_half_tan_p25_gc9e-3_ts30_cs330_l0.25_delta-0.625.cs
 
 t = pp["time"]*1e6
 energy = {
-  "Strain energy": pp["strain_energy"],
-  "Kinetic energy": pp["kinetic_energy"],
   "Fracture energy": pp["fracture_energy"],
+  "Strain energy": pp["strain_energy"] + pp["strain_energy_p"],
+  "Kinetic energy": pp["kinetic_energy"] + pp["kinetic_energy_p"],
 }
 
-ax.stackplot(t, energy.values(), labels=energy.keys(), alpha=0.8)
-ax.plot(t, pp["external_work"], label="External work")
-# ax.plot(t, pp["fracture_energy"])
+# ax.stackplot(t, energy.values(), labels=energy.keys(), alpha=0.6)
+# ax.plot(t, pp["external_work"], label="External work")
+ax.plot(t, pp["fracture_energy"], label="Fracture energy")
+# ax.plot(t, pp["strain_energy"], label="Strain energy (glass)")
+# ax.plot(t, pp["strain_energy_p"], label="Strain energy (putty)")
+# ax.plot(t, pp["kinetic_energy"], label="Kinetic energy (glass)")
+# ax.plot(t, pp["kinetic_energy_p"], label="Kinetic energy (putty)")
 
 ax.set_xlabel("Time ($\mu$s)")
-ax.set_ylabel("Fracture energy (mJ)")
-ax.legend()
+ax.set_ylabel("Energy (mJ)")
+ax.legend(loc='upper left')
 # %%
-fig.savefig('pp_energy.png')
+# fig.savefig('./figure/pp_energy.png')
+# fig.savefig('pp_fracture_energy.png')
+# fig.savefig('pp_strain_energy.png')
+# fig.savefig('pp_kinetic_energy.png')
 # %%
